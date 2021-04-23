1a77d785d9a5accabf128450e9f59c440a8759fe
Oli <olivier@innosonian.com>
2021-04-23 16:02:56 +0900

    payler banner v1 + stat structure v.001

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@material-ui/core';
+import { Container, createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PlayerBanner from './components/PlayerBanner';
@@ -7,17 +7,31 @@ import TagList from './components/TagList'
 
 export default function App() {
   const [input, setInput] = useState('');
+  const classes = useStyles();
 
 
   return (
     <View style={styles.container}>
-            <SearchAppBar 
+      <SearchAppBar
       />
-<Container>
+      <Container>
 
-      <TagList />
-      <PlayerBanner/>
+        <TagList />
+        <PlayerBanner />
       </Container>
+      <div className={classes.line}>
+        <Container>
+          <Grid container spacing={3} className={classes.statsDataContainer}>
+            <Grid item container direction="row" spacing={3} xs={2}>
+              <Grid item xs={12}> top left</Grid>
+              <Grid item xs={12}> bottom left</Grid>
+            </Grid>
+            <Grid item xs={10}>
+              right
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
     </View>
   );
 }
@@ -27,4 +41,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
+
 });
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    line: {
+      borderTop: "1px solid #d8d8d8"
+
+    },
+    statsDataContainer: {
+      display: "flex",
+      padding: "15px",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+  })
+
+)
--- components/PlayerBanner.tsx
+++ components/PlayerBanner.tsx
@@ -4,61 +4,83 @@ import { Box, Grid, Typography } from '@material-ui/core';
 import Image from 'material-ui-image'
 
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
-  root: {
-    flexGrow: 1,
-    borderBottom:"1px",
-    padding : "15px",
+    createStyles({
+        root: {
+            flexGrow: 1,
+            borderBottom: "1px",
+            padding: "15px",
 
 
-  },
-  name : {
-    fontFamily: "Helvetica",
-    fontSize: "20px",
-    fontWeight: "bold",
-    letterSpacing: "-0.77px",
-    color: "#242929",
-  },
-  desc : {
-    fontFamily:" Helvetica",
-    fontSize: "11px",
-    color: "#657070",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "normal",
-    letterSpacing: "normal"
-
-  },
-  image : {
-    backgroundImage: "url(https://opgg-static.akamaized.net/images/profile_icons/profileIcon1625.jpg)",
-    backgroundPosition: "center", /* Center the image */
-    backgroundRepeat: "no-repeat", /* Do not repeat the image */
-    backgroundSize: "85%" /* Resize the background image to cover the entire container */
-  }
-}));
+        },
+        name: {
+            fontFamily: "Helvetica",
+            fontSize: "20px",
+            fontWeight: "bold",
+            letterSpacing: "-0.77px",
+            color: "#242929",
+        },
+        desc: {
+            fontFamily: " Helvetica",
+            fontSize: "11px",
+            color: "#657070",
+        },
+        textGrid:{
+            height:"100%"
+        },
+        badge: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 100,
+            position: 'absolute',
+        },
+        image: {
+            backgroundImage: "url(https://opgg-static.akamaized.net/images/profile_icons/profileIcon1625.jpg)",
+            backgroundPosition: "center", /* Center the image */
+            backgroundRepeat: "no-repeat", /* Do not repeat the image */
+            backgroundSize: "85%" /* Resize the background image to cover the entire container */
+        },
+        level: {
+            zIndex: 100,
+            position: 'absolute',
+            color: "#eabd56"
+            //  marginTop: "-14px",
+            //  backgroundImage: "url('../assets/bglevelbox.png')",
+        },
+        portrait: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-end",
+        }
+    }));
 
 export default function PlayerBanner() {
     const classes = useStyles();
     return (
-      <div >
+        <div >
 
-      <Grid
-  container
-  direction="row"
-  justify="Left"
-  alignItems="left"
-className={classes.root} spacing={3} >
+            <Grid container direction="row" className={classes.root} spacing={5} >
+                <Grid item xs={1} >
+                    <Grid container item  xs={12} className={classes.portrait} >
 
-<Grid item xs={1}> <Image className={classes.image} src="https://opgg-static.akamaized.net/images/borders2/challenger.png" /> </Grid>
-          <Grid container direction="column" spacing={1} item xs={11}>
-          <Grid item xs={12} className={classes.name}>Name</Grid>
-          <Grid item xs={12} ><Typography className={classes.desc} component='div'>Normal text <Box fontWeight='fontWeightBold' display='inline'>medium font weight text</Box> and some more normal text</Typography>
-</Grid>
+                        <img className={classes.image} src="https://opgg-static.akamaized.net/images/borders2/challenger.png" />
+                        <Grid item xs={12} className={classes.badge}>
+                            <img src={require('../assets/bglevelbox.png')} />
 
+                            <Typography className={classes.level} >68</Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid container direction="column"  spacing={3} item xs={11} >
+                    <Grid item xs={1}  className={classes.name}>Name
+                    </Grid>
+                    <Grid item xs={8} >
+                        <Typography className={classes.desc} component='div'>Normal text <Box fontWeight='fontWeightBold' display='inline'>medium font weight text</Box> and some more normal text</Typography>
+                    </Grid>
+
+                </Grid>
             </Grid>
-          </Grid>
-          
+
         </div>
-        );
+    );
 }
